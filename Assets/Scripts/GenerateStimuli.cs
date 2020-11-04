56a5cfd884b8b2de392a76f1cde179261ffadb71
Unknown <aznnoodle101@hotmail.com>
2020-11-03 20:29:14 -0800

    Done
    done

--- Assets/Scripts/GenerateStimuli.cs
+++ Assets/Scripts/GenerateStimuli.cs
@@ -13,18 +13,24 @@ public class GenerateStimuli : MonoBehaviour {
     public float blueDist2;
     private float ratio1;
     private float ratio2;
-    private bool s;
     private Vector3 originalLocalScale;
+    private bool resizeToggle;
+    private bool appearToggle;
+    private float timer;
 
     private void Start() {
         //blueSphere1.transform.localScale *= ratio / Vector3.Distance(blueSphere1.transform.position, this.transform.position);
         //blueSphere2.transform.localScale *= ratio / Vector3.Distance(blueSphere2.transform.position, this.transform.position);
 
         originalLocalScale = blueSphere1.transform.localScale;
+        timer = 0;
+        resizeToggle = false;
+        appearToggle = true;
     }
 
     private void Update() {
-        if (Input.GetKeyDown(KeyCode.S)) { s = !s; }
+        if (Input.GetKeyDown(KeyCode.R)) { resizeToggle = !resizeToggle; }
+        if (Input.GetKeyDown(KeyCode.F)) { appearToggle = !appearToggle; } // going from visible to invisible
     }
 
     public void FixedUpdate() {
@@ -42,8 +48,35 @@ public class GenerateStimuli : MonoBehaviour {
         // r1 / d1 = r2 / d2
         ratio = redSphere.transform.localScale.x / redDist;
 
-        if (s) { Resize(); }
+        if (resizeToggle) { Resize(); }
         else { blueSphere2.transform.localScale = blueSphere1.transform.localScale = originalLocalScale; }
+
+        // Make Spheres appear and disappear
+        if (redSphere.activeSelf && !appearToggle) {
+            redSphere.SetActive(false);
+        }
+        else if (!redSphere.activeSelf && blueSphere1.activeSelf && !appearToggle) {
+            // Red sphere is invisible, and button is pressed to activate disappearance
+            timer += Time.fixedDeltaTime; // count to 2 seconds
+            if(timer >= 2) {
+                blueSphere1.gameObject.SetActive(false);
+                blueSphere2.gameObject.SetActive(false);
+                timer = 0;
+            }
+        }
+        else if (!redSphere.activeSelf && !blueSphere1.activeSelf && appearToggle) {
+            // Red and Blue spheres are invisible, and button is pressed to activate reappearance
+            redSphere.SetActive(true);
+        }
+        else if(redSphere.activeSelf && !blueSphere1.activeSelf && appearToggle) {
+            // Blue spheres are invisible, and button is pressed to activate reappearane
+            timer += Time.fixedDeltaTime;
+            if(timer >= 2) {
+                blueSphere1.gameObject.SetActive(true);
+                blueSphere2.gameObject.SetActive(true);
+                timer = 0;
+            }
+        }
     }
 
     private void Resize() {
